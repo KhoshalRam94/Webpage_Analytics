b487d9d41990240d73e336b430ef0c72ade05b1e
Khoshal Ram <130228791+KhoshalRam94@users.noreply.github.com>
2024-08-02 00:23:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import streamlit-analytics
 
+with streamlit_analytics.track():
+  st.write("Write your Name.")
+  st.checkbox("Which is your favourite",["DS","DE","AI","ML"])
+  st.button("Click me")
