3685540d88314918299d236012d0a379090b4508
Khoshal Ram <130228791+KhoshalRam94@users.noreply.github.com>
2024-08-02 00:34:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import streamlit_analytics
 
-with streamlit_analytics.track():
-  st.text_input("Write your Name.")
-  st.selectbox("Which is your favourite",["DS","DE","AI","ML"])
-  st.button("Click me")
+st.text_input("Write your Name.")
+st.selectbox("Which is your favourite",["DS","DE","AI","ML"])
+st.button("Click me")
